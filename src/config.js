8310bbe59f5f57ccb8348e6ae65d5063841ce683
GomezManuel <0321101422@ut-tijuana.edu.mx>
2024-08-26 23:36:51 -0700

    cambio de la db

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ config();
 
 export const database = {
    connectionLimit:10,
-   host:process.env.DATABASE_HOST||"b8xsgydpu3fo7upnjham-mysql.services.clever-cloud.com",
-   user:process.env.DATABASE_USER||"uqahecg8ni9wx5x2",
-   password:process.env.DATABASE_PASSWORD||"6AEpC24iIGy3t2BhuKCq",
-   database:process.env.DATABASE_NAME||"b8xsgydpu3fo7upnjham"
+   host:process.env.DATABASE_HOST||"b6nxnbp46dpfkogm8ltb-mysql.services.clever-cloud.com",
+   user:process.env.DATABASE_USER||"u8igleicnt9uprqz",
+   password:process.env.DATABASE_PASSWORD||"9KduH4T8OWAznLSpjDlN",
+   database:process.env.DATABASE_NAME||"b6nxnbp46dpfkogm8ltb"
 }
 
 export const port=process.env.PORT|| 3000;
